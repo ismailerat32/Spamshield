1442f6f5fd88e526dd6aa3988de38f84d179fbf4
ismail erat <ismail.erat32@gmail.com>
2026-03-25 20:34:03 +0300

    SpamShield Final Release v1.0 - production ready, env system, landing page, improved analyzer and dashboard

--- mailer.py
+++ mailer.py
@@ -2,13 +2,15 @@ import smtplib
 from email.message import EmailMessage
 
 def send_mail(smtp_host, smtp_port, smtp_user, smtp_pass, to_email, subject, body):
+    smtp_pass = smtp_pass.replace(" ", "").replace("\u00a0", "")
+
     msg = EmailMessage()
     msg["Subject"] = subject
     msg["From"] = smtp_user
     msg["To"] = to_email
     msg.set_content(body)
 
-    with smtplib.SMTP(smtp_host, smtp_port) as server:
+    with smtplib.SMTP(smtp_host, int(smtp_port)) as server:
         server.starttls()
         server.login(smtp_user, smtp_pass)
         server.send_message(msg)
